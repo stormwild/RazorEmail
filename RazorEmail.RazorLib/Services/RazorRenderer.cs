19d72d25f609789d6d754440cc712c38aa51ddc5
Alexander Torrijos <artorrijos@gmail.com>
2018-07-18 22:17:34 +0800

    Updates

--- RazorEmail.RazorLib/Services/RazorRenderer.cs
+++ RazorEmail.RazorLib/Services/RazorRenderer.cs
@@ -6,13 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using System.Linq;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 
 namespace RazorEmail.Lib.Services
 {
     public class RazorRenderer : IRazorRenderer
     {
         private IRazorViewEngine _viewEngine;
-        private ITempDataProvider _dataProvider;
+        private ITempDataProvider _tempDataProvider;
         private IServiceProvider _serviceProvider;
 
         public RazorRenderer(
@@ -21,14 +26,56 @@ namespace RazorEmail.Lib.Services
             IServiceProvider serviceProvider)
         {
             _viewEngine = viewEngine;
-            _dataProvider = dataProvider;
+            _tempDataProvider = dataProvider;
             _serviceProvider = serviceProvider;
         }
 
-        public Task<string> RenderAsync<TModel>(string view, TModel model)
+        public async Task<string> RenderAsync<TModel>(string viewName, TModel model)
         {
             var actionContext = GetActionContext();
-            return null;
+            var view = FindView(actionContext, viewName);
+
+            using (var output = new StringWriter())
+            {
+                var viewContext = new ViewContext(
+                    actionContext,
+                    view,
+                    new ViewDataDictionary<TModel>(
+                        metadataProvider: new EmptyModelMetadataProvider(),
+                        modelState: new ModelStateDictionary()
+                    ) {
+                        Model = model
+                    },
+                    new TempDataDictionary(actionContext.HttpContext, _tempDataProvider),
+                    output,
+                    new HtmlHelperOptions()
+                );
+
+                await view.RenderAsync(viewContext);
+
+                return output.ToString();
+            }
+
+        }
+
+        private IView FindView(ActionContext actionContext, string viewName)
+        {
+            var getViewResult = _viewEngine.GetView(executingFilePath: null, viewPath: viewName, isMainPage: true);
+            if(getViewResult.Success) {
+                return getViewResult.View;
+            }
+
+            var findViewResult = _viewEngine.FindView(actionContext, viewName, isMainPage: true);
+            if(findViewResult.Success) {
+                return findViewResult.View;
+            }
+            
+            var searchedLocations = getViewResult.SearchedLocations.Concat(findViewResult.SearchedLocations);
+            var errorMessage = string.Join(
+                Environment.NewLine,
+                new[] { $"Unable to find view '{viewName}'. The following locations were searched:" }.Concat(searchedLocations)
+            );
+            throw new InvalidOperationException(errorMessage);
         }
 
         private ActionContext GetActionContext() {
